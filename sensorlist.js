f4ef25027ddb32c15415249026c6dca7ef27bc79
jxmot <webdev@stupiddog.net>
2017-12-30 21:59:36 -0600

    added high/low limts for event triggers

--- sensorlist.js
+++ sensorlist.js
@@ -1,9 +1,9 @@
 module.exports = {
     list : [
-        {name:"ESP_49EC8B",loc:"BLU"},
-        {name:"ESP_49F542",loc:"RED"},
-        {name:"ESP_49EB40",loc:"GRN"},
-        {name:"ESP_49ECCD",loc:"WHT"},
+        {name:"ESP_49EC8B",loc:"BLU",low_limit:40,high_limit:95},
+        {name:"ESP_49F542",loc:"RED",low_limit:40,high_limit:95},
+        {name:"ESP_49EB40",loc:"GRN",low_limit:40,high_limit:95},
+        {name:"ESP_49ECCD",loc:"WHT",low_limit:40,high_limit:95},
         {name:"END",loc:"END"}
     ]
 };
