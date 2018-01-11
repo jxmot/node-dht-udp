1ed1499b4684e5611287793a63cd0259a386a482
jxmot <webdev@stupiddog.net>
2018-01-11 04:14:07 -0600

    comments

--- firebase/firebase-rest-config.js
+++ firebase/firebase-rest-config.js
@@ -1,11 +1,14 @@
 module.exports = {
     // components for building firebase API calls - 
+    // (see example_firebase-config.js)
     CONFIG : require(__dirname + '/_firebase-config.js'),
 
     /*
         Path components used in creating full paths to the data
         we want to access. This would be specific to the application
         and would change as needed.
+
+        (see example_firebase-paths.js)
     */
     PATHS : require(__dirname + '/_firebase-paths.js'),
 
