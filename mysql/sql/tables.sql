5a649115409bf042b4f0af1dbd28deb1e51decb6
jxmot <webdev@stupiddog.net>
2018-01-21 18:17:07 -0600

    improved comments

--- mysql/sql/tables.sql
+++ mysql/sql/tables.sql
@@ -1,34 +1,45 @@
+/*
+    A collection of convienient SQL statements :
+
+        * database create
+        * table creation
+        * test insertions & deletions
+*/
+
 create database sensornet;
 
 /*
-1484956740000 = Friday, January 20, 2017 5:59:00 PM GMT-06:00
+    The 'tstamp' column is used as the primary key. This means that it
+    can be used for - 
 
-1516492740000 = Saturday, January 20, 2018 5:59:00 PM GMT-06:00
+        * Selecting a range of rows from the table
+        * Deleting a range of rows from the table
 
-Where a is the smallest number and b is the largest number
-SELECT RAND()*(b-a)+a;
+    For example, 'tstamp' contents could appear as - 
 
+        1484956740000 = Friday, January 20, 2017 5:59:00 PM GMT-06:00
+        
+        1516492740000 = Saturday, January 20, 2018 5:59:00 PM GMT-06:00
 
-SELECT FLOOR(RAND()*(1516492740000-1484956740000)+1484956740000);
+    Examples of time durations (in millseconds) -
 
-SELECT FLOOR(RAND()*(1516492740-1484956740)+1484956740);
-
- 1 day  =   86400000
-
- 5 days =  432000000
-
-10 days =  864000000
-
-14 days = 1209600000
-
-30 days = 2592000000
-
-60 days = 5184000000
-
-90 days = 7776000000
-
+         1 day  =   86400000
+        
+         5 days =  432000000
+        
+        10 days =  864000000
+        
+        14 days = 1209600000
+        
+        30 days = 2592000000
+        
+        60 days = 5184000000
+        
+        90 days = 7776000000
 */
-
+/*
+    Sensor Status Table
+*/
 create table sensornet.status (
     dev_id varchar(64) not null,
     status varchar(16) not null,
@@ -36,14 +47,33 @@ create table sensornet.status (
     tstamp bigint(16) not null, primary key (tstamp) 
 );
 
+/*
+    Using a timestamp as the primary key...
+
+    To see how this works, experiment with the following SQL statements :
+
+        SELECT FLOOR(RAND()*(1516492740000-1484956740000)+1484956740000);
+        SELECT FLOOR(RAND()*(1516492740-1484956740)+1484956740);
+
+    Where a is the smallest number and b is the largest number - 
+
+        SELECT RAND()*(b-a)+a;
+
+    
+*/
 insert into sensornet.status 
 (dev_id,status,tstamp)
 values ("ESP_49EB40", "APP_READY", FLOOR(RAND()*(1516492740000-1484956740000)+1484956740000));
 
+/*
+    this will delete a range of rows where their timestamp is less than a
+    start date/time minus a duration of time
+*/
 DELETE FROM sensornet.status where tstamp < (1516492740000 - 7776000000);
 
-
-
+/*
+    Sensor Data Table
+*/
 create table sensornet.data (
     dev_id varchar(64) not null,
     seq int(6) not null,
