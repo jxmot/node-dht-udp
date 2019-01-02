23996d9a83a50d4c58c334ecc7bd57b210b8ced8
jxmot <webdev@stupiddog.net>
2019-01-02 15:21:17 -0600

    minor changes for aws integration

--- api/alexacfg.js
+++ api/alexacfg.js
@@ -6,18 +6,20 @@ module.exports = {
     // requests from the alexa skill
     host : '192.168.0.7',
     port : 80,
+    _port : 8080,
     // devices must match the creator of the
     // data file. 
     devices: {
     //  Device ID    Proper Name      Query Path
-        'ESP_39F542':['Den',            'den'],
-        'ESP_49EB40':['Master Bedroom', 'mbr'],
-        'ESP_49ECCD':['Living Room',    'lr'],
-        'ESP_49EC8B':['Office',         'ofc']
+        'ESP_39F542':['den',            'den'],
+        'ESP_49EB40':['master bedroom', 'mbr'],
+        'ESP_49ECCD':['living room',    'lr'],
+        'ESP_49EC8B':['office',         'ofc']
     },
     // path relative to application location
     watch: '../datashare/sensorlast.json',
     // this must match the ID provided in the 
     // requests from the alexa skill
-    axid: '123456'
+    axid: '123456',
+    _axid: 'EvW4{.7sgAzf$E!K'
 };
